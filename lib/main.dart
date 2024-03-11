7185eb33ee4f93fc96d09ec7dbcf35ee149c34c4
Nadzib <nadzib22@gmail.com>
2024-03-11 15:32:36 +0700

    p

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(new MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: Scaffold(
+
+        appBar: AppBar(
+          title: const Text("Aplikasi hello world"),
+        ),
+        body: const Text("Hello world"),
+        
+      ),
+    );
   }
 }
